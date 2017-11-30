5fa7c1eb51aee1a6a89339c9b1cd9343bbb9af3f
appleMini <s1298web@163.com>
2017-11-30 17:21:43 +0800

    Initial commit

---
+++ Example/SkyboxUtil/SPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SPAppDelegate.h
+//  SkyboxUtil
+//
+//  Created by appleMini on 11/30/2017.
+//  Copyright (c) 2017 appleMini. All rights reserved.
+//
+
+@import UIKit;
+
+@interface SPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
